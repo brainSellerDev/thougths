0f42d9ffb34344e1a0f994406355a89278bd8552
Brain Seller <kesamirov1997@gmail.com>
2023-09-08 12:52:36 +0300

    add UI library antdv

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@
 <template>
   <main>
     <!-- CODE  -->
+    <h1 class="title">THOUGHTS</h1>
   </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+$titleColor:red;
+.title{
+  color: $titleColor;
+}
+</style>
