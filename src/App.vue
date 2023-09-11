91b298f5eeaeb1c892dcee0037d6864d63bf7703
Brain Seller <kesamirov1997@gmail.com>
2023-09-11 12:17:49 +0300

    import antdv components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { UserOutlined, InfoCircleOutlined } from '@ant-design/icons-vue';
+import { ref } from 'vue';
+const userName = ref<string>('');
+</script>
 
 <template>
-  <main>
-    <!-- CODE  -->
-    <h1 class="title">THOUGHTS</h1>
-  </main>
+  <div class="components-input-demo-presuffix">
+    <a-input v-model:value="userName" placeholder="Basic usage">
+      <template #prefix>
+        <user-outlined />
+      </template>
+      <template #suffix>
+        <a-tooltip title="Extra information">
+          <info-circle-outlined style="color: rgba(0, 0, 0, 0.45)" />
+        </a-tooltip>
+      </template>
+    </a-input>
+    <br />
+    <br />
+    <a-input prefix="￥" suffix="RMB" />
+  </div>
 </template>
 
 <style lang="scss" scoped>
